ffe3cb0969a8fab8df6774acabde3a68dd82b991
ainmatzam <95876304+ainmatzam@users.noreply.github.com>
2023-01-12 15:56:51 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      // colors: {
+      //   standBlue: "hsla(208, 100%, 50%, 1)",
+      //   darkBlue: "hsl(203, 100%, 12%)",
+      // },
+      colors: {
+        dim: {
+          50: "#5F99F7",
+          100: "#5F99F7",
+          200: "#38444d",
+          300: "#202e3a",
+          400: "#253341",
+          500: "#5F99F7",
+          600: "#5F99F7",
+          700: "#192734",
+          800: "#162d40",
+          900: "#15202b",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
